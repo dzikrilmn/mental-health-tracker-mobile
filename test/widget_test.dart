20174ca012c370dd18cf74adc0e57f36615d9776
dzikrilmn <dzikri.ilmansyah@gmail.com>
2024-11-13 15:00:47 +0700

    tutorial 8

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,6 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:mental_health_tracker/main.dart';
+import 'package:mental_health_tracker/screens/menu.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
